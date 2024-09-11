4b2f54b11388110e9981088c5ae62f84a2f13d57
Ayush-Sharma-007 <ayush120410sharma@gmail.com>
2024-09-12 01:14:37 +0530

    created contact page

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -1,8 +1,5 @@
 import localFont from "next/font/local";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,9 +23,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
         {children}
-        <Footer/>
       </body>
     </html>
   );
